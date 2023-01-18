d564f79c52c09a845e74df0e4eeef3d3f483edb1
Tadeo <tmenichel@protonmail.com>
2023-01-18 12:35:35 -0500

    Base Shooter game push

---
+++ ShooterGame/Source/ShooterGame/Public/PredictShooterMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "PredictShooterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API UPredictShooterMovementComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+	
+};
